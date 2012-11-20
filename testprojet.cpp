c0a206402d3e9367ccf6b54b8a346e85a4ed3a12
edelans <edelans@gmail.com>
2012-11-20 18:16:02 +0100

    correction e avec accents

--- testprojet.cpp
+++ testprojet.cpp
@@ -8,7 +8,7 @@
 using namespace cimg_library;
 using namespace std;
 
-#define LONGUEUR_MAX		24
+#define LONGUEUR_MAX	24
 #define LARGEUR_MAX		18
 #define TAILLE_CASE		32
 
@@ -26,7 +26,7 @@ public:
 
 void drawing::initialize(const string file) const
 {
-	ifstream is(file); // tous les num�ros codant les cases dans le fichier sont lues
+	ifstream is(file); // tous les numeros codant les cases dans le fichier sont lues
 	if(!is) 
         {       
             cerr << "Impossible d'ouvrir le fichier !" << endl;
@@ -34,16 +34,16 @@ void drawing::initialize(const string file) const
 	string s;
 	int longueur_niveau;
 	getline(is,s);
-	longueur_niveau = (s.size()+1)/2; // on r�cup�re la taille du niveau (nombre de cases sur une ligne)
+	longueur_niveau = (s.size()+1)/2; // on recupere la taille du niveau (nombre de cases sur une ligne)
 	is.seekg(0,ios::beg);
 
-	int d�calage_X = (LONGUEUR_MAX - longueur_niveau - 3)*TAILLE_CASE/2;
-	int d�calage_Y = (LARGEUR_MAX - longueur_niveau - 3)*TAILLE_CASE/2;
+	int decalage_X = (LONGUEUR_MAX - longueur_niveau - 3)*TAILLE_CASE/2;
+	int decalage_Y = (LARGEUR_MAX - longueur_niveau - 3)*TAILLE_CASE/2;
 
 	string nombre;
 	int NumeroCase = 1;
 
-	while (is >> nombre) // parcours de chaque case cod�e dans le fichier de niveau
+	while (is >> nombre) // parcours de chaque case codee dans le fichier de niveau
 	{
 		cout << nombre << endl;
 		int value =	atoi(nombre.c_str());
@@ -54,16 +54,16 @@ void drawing::initialize(const string file) const
 		switch (value)
 		{
 		case 1:
-			game.draw_image((NumeroColonne - 1/2)*TAILLE_CASE + d�calage_X,(NumeroLigne - 1/2)*TAILLE_CASE + d�calage_Y,sol);
+			game.draw_image((NumeroColonne - 1/2)*TAILLE_CASE + decalage_X,(NumeroLigne - 1/2)*TAILLE_CASE + decalage_Y,sol);
 			break;
 		case 2:
-			game.draw_image((NumeroColonne - 1/2)*TAILLE_CASE + d�calage_X,(NumeroLigne - 1/2)*TAILLE_CASE + d�calage_Y,murdestr);
+			game.draw_image((NumeroColonne - 1/2)*TAILLE_CASE + decalage_X,(NumeroLigne - 1/2)*TAILLE_CASE + decalage_Y,murdestr);
 			break;
 		case 3:
-			game.draw_image((NumeroColonne - 1/2)*TAILLE_CASE + d�calage_X,(NumeroLigne - 1/2)*TAILLE_CASE + d�calage_Y,mur);
+			game.draw_image((NumeroColonne - 1/2)*TAILLE_CASE + decalage_X,(NumeroLigne - 1/2)*TAILLE_CASE + decalage_Y,mur);
 			break;
 		default:
-			cerr << "Fichier non conforme au type attendu : la case num�ro " << NumeroCase << " ne correspond pas � un type de terrain dans le champ attendu" << endl;
+			cerr << "Fichier non conforme au type attendu : la case numero " << NumeroCase << " ne correspond pas � un type de terrain dans le champ attendu" << endl;
 			break;
 		}
 		NumeroCase++;
