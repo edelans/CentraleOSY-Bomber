714f7335fe1c9e61548725da276815af5c698529
edelans <edelans@gmail.com>
2012-11-29 16:54:47 +0100

    correction indentation bombe.h

--- Bomberman/bombe.h
+++ Bomberman/bombe.h
@@ -18,15 +18,15 @@ class Bombe
 public: 
     Bombe();
     ~Bombe();
-    void	explose();    // pour détruire les blocs destructibles, tuer les personnages  //
+	void	explose();    // pour détruire les blocs destructibles, tuer les personnages  //
 	void	afficheExplosion();  // pour remplacer temporairement les blocs autour de la bombe par l'image d'explosion
 	void	enleverAffichageExplosion(); // pour nettoyer la map des images d'explosion
 	
 
 
 private:
-    int		m_timer;   // compte à rebours entre moment ou elle est posée et explosion
-    int		m_portee;  // nombre de blocs détruits dans chaque direction lors de l'explosion
+	int		m_timer;   // compte à rebours entre moment ou elle est posée et explosion
+	int		m_portee;  // nombre de blocs détruits dans chaque direction lors de l'explosion
 	bool	m_isExploding;  // correspond à l'etat ou les images de l'explosion sont affichées
 	bool	m_isExploded;   // correspond a l'etat ou on a arrete d'afficher les images d'explosion
 };
